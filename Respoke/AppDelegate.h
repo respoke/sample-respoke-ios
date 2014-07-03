3d260038b872e40272195814e6fed13896a9a40d
Jason Adams <jason@ninjanetic.com>
2014-07-03 17:48:55 -0500

    Skeleton universal app.

---
+++ Respoke/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Respoke
+//
+//  Created by Jason Adams on 7/3/14.
+//  Copyright (c) 2014 Ninjanetic Design Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Respoke/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Respoke
+//
+//  Created by Jason Adams on 7/3/14.
+//  Copyright (c) 2014 Ninjanetic Design Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
