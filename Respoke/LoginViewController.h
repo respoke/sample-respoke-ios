e5aa1ca88c60424fee6dda5b527301452523f13a
Jason Adams <jason@ninjanetic.com>
2014-07-04 09:29:58 -0500

    Style the login view.

--- Respoke/LoginViewController.h
+++ Respoke/LoginViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface LoginViewController : UIViewController
 
 @end
--- Respoke/LoginViewController.m
+++ Respoke/LoginViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2014 Ninjanetic Design Inc. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LoginViewController.h"
 
-@interface ViewController ()
+@interface LoginViewController ()
 
 @end
 
-@implementation ViewController
+@implementation LoginViewController
 
 - (void)viewDidLoad
 {
