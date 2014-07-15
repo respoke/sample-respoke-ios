11de1504bf9d2846300374d630e72a1e6c4bd2ad
Jason Adams <jason@ninjanetic.com>
2014-07-15 13:37:11 -0500

    Animate chat toolbar location.

--- Respoke/Respoke/ChatTableViewController.m
+++ Respoke/Respoke/ChatTableViewController.m
@@ -31,8 +31,12 @@
     [conversation addMessage:@"Hi there!" from:self.username];
     [conversation addMessage:@"Top of the morning to you, fine sir. Top of the morning to you, fine sir. Top of the morning to you, fine sir." from:self.endpoint.endpointID];
     [conversation addMessage:@"Thanks bro" from:self.username];
+
     remotePrototype = [self.tableView dequeueReusableCellWithIdentifier:@"RemoteMessage"];
     localPrototype = [self.tableView dequeueReusableCellWithIdentifier:@"LocalMessage"];
+
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
 }
 
 
@@ -128,6 +132,29 @@
 - (IBAction)callAction
 {
 
+}
+
+
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+    NSTimeInterval animationDuration = [[[notification userInfo] valueForKey: UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    CGRect keyboardBounds = [(NSValue *)[[notification userInfo] objectForKey: UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+    
+    [UIView animateWithDuration:animationDuration animations:^(void){
+        self.bottomConstraint.constant = keyboardBounds.size.height;
+        [self.view layoutIfNeeded];
+    } completion:nil];
+}
+
+
+- (void)keyboardWillHide:(NSNotification *)notification
+{
+    NSTimeInterval animationDuration = [[[notification userInfo] valueForKey: UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    [UIView animateWithDuration:animationDuration animations:^(void){
+        self.bottomConstraint.constant = 0;
+        [self.view layoutIfNeeded];
+    } completion:nil];
 }
 
 
